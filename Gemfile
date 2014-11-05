29600a01fa5c86bdaf6d1c1961abe0a391ec2eb3
Juan Bonfante <jbonfante@enova.com>
2014-11-05 11:04:32 -0600

    My horrible first pass at making these tests pass
    Module [enova-2014-nov]:
      + scope_settings.xml
      * bottles.rb
      * bottles_test.rb
      + .name
      + compiler.xml
      + encodings.xml
      + misc.xml
      + modules.xml
      + vcs.xml
      * Gemfile
      * Gemfile.lock
      + enova-2014-nov.iml

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'minitest', '~> 5.2'
+gem 'activesupport', require: 'active_support/all'
--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,6 +1,56 @@
 # Feel free to delete the instructions once you get going
+require 'active_support/all'
 puts instructions(__FILE__)
 
+
 class Bottles
+    attr_accessor :word
+
+  def initialize(word='bottle')
+    @word = word
+  end
+
+  def verse(number)
+    bottle = "bottle".pluralize(number)
+    bottle_less_one = "bottle".pluralize(number-1)
+
+    if number.eql?(0)
+      return no_bottles
+    end
+
+
+    last_verse = "Take one down and pass it around, #{number - 1} #{bottle_less_one} of beer on the wall."
+
+    if (number-1).eql?(0)
+      last_verse = "Take it down and pass it around, no more bottles of beer on the wall."
+    end
+
+    <<-VERSE
+#{number} #{bottle} of beer on the wall, #{number} #{bottle} of beer.
+#{last_verse}
+    VERSE
+  end
+
+  def verses(*numbers)
+    verses = []
+    numbers.each {|number| verses << verse(number) }
+    verses.join(empty_line)
+  end
+
+  private
+  def no_bottles
+    <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+  end
+
+
+  def empty_line
+    <<-EMPTY
+
+    EMPTY
+  end
+
 
 end
